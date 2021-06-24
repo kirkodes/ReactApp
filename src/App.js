030441bec1f41fab061db7991539cef963e464b6
Kirsten Purpura <purpurakirsten@gmail.com>
2021-06-24 18:00:26 -0400

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+import Footer from './components/site/Footer';
+import Header from './components/site/Header';
+import Sidebar from './components/site/Sidebar';
+import {
+  BrowserRouter as Router // browser router renamed to router (lazy and easier) + this is how you can rename an import ; router must be declared in App.js or index.js or our routing wont work
+} from 'react-router-dom';
+import FunctionalComponentDemo from './components/concepts/FunctionalComponents/FunctionalComponentDemo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'App'>
+      <FunctionalComponentDemo/>
     </div>
   );
-}
+};
 
+// function App() { // header and footer will stay the same regardless of what "page" we're on; router inside of these means they won't be affected by any route changes at all. header and footer will show up regardless of what route we're at; the only thing that changes is the main content of our page which is controlled by <Sidebar />
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Router>
+//         <Sidebar />
+//       </Router>
+//       <Footer />
+//     </div>
+//   );
+// };
 export default App;
---
+++ src/components/site/Sidebar.js
@@ -0,0 +1,30 @@
+import {
+    Route, // determine what will be shown on our screen at the path
+    Link, // actually take us to the path
+    Switch // only one of the conditions is true at a time, so only one part of our application shows at a time
+} from 'react-router-dom';
+import Home from './Home';
+import Resources from './Resources';
+
+const Sidebar = () => {
+    return(
+        <div className='sidebar'>
+            <div className='sidebar-list-styling'>
+                <ul className='sidebar-list list-unstyled'>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/resources'>Resources</Link></li>
+                    <li><Link to='/functionalcomponent'>Functional Component</Link></li>
+                </ul>
+            </div>
+            <div className='sidebar-route'>
+                <Switch>
+                    <Route exact path='/home'><Home /></Route>
+                    <Route exact path='/resources'><Resources /></Route>
+                    <Route exact path='/'><Home /></Route>
+                </Switch>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
