e46322b502f2b0a1ae456bf7195416b3122fd011
Kirsten Purpura <purpurakirsten@gmail.com>
2021-06-27 14:36:07 -0400

    NYT App in React.js

--- src/components/apps/nyt-app/NytApp.js
+++ src/components/apps/nyt-app/NytApp.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import NytResults from './NytResults';
 const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'; // api end point
 const key = '4PKvt7YX2uDsn5xem9VJLmFQPSjPMxM7';
 
@@ -7,7 +7,7 @@ const NytApp = () => {
     const [ search, setSearch ] = useState('');
     const [ startDate, setStartDate ] = useState('');
     const [ endDate, setEndDate ] = useState('');
-    const [ pageNumber, setPageNumber ] = useState('');
+    const [ pageNumber, setPageNumber ] = useState(0);
     const [ results, setResults ] = useState([]);
 
     const fetchResults = () => {
@@ -21,13 +21,44 @@ const NytApp = () => {
             .catch(err => console.log(err));
     };
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setPageNumber(0);
+        fetchResults();
+    }
+
+    const changePageNumber = (event, direction) => {
+        event.preventDefault();
+        if(direction === 'down') {
+            if(pageNumber > 0) {
+                setPageNumber(pageNumber + 1);
+                fetchResults();
+            }
+        }
+        if(direction === 'up') {
+            setPageNumber(pageNumber +1);
+            fetchResults();
+        }
+    };
+
     return(
         <div className='main'>
             <div className='mainDiv'>
-                <form onSubmit={ }>
+                <form onSubmit={(e) => handleSubmit(e) }>
                     <span>Enter a single search term (required) : </span>
-                    <input type='text' name='search' onChange={ } required />
+                    <input type='text' name='search' onChange={(e) => setSearch(e.target.value) } required />
+                    <br />
+                    <span>Enter a start date: </span>
+                    <input type='date' name="startDate" pattern="[0-9]{8}" onChange={(e) => setStartDate(e.target.value) } />
+                    <br />
+                    <span>Enter an end date: </span>
+                    <input type='date' name="endDate" pattern="[0-9]{8}" onChange={(e) => setEndDate(e.target.value) } />
+                    <br />
+                    <button className="submit">Submit search</button>
                 </form>
+                {
+                    results.length > 0 ? <NytResults results={results} /> : null
+                }
             </div>
         </div>
     )
